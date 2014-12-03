2a427b4ea2ad057ac2dc2826332d9c6fb5a94063
Jess Yarbrough <jessyarbrough@hotmail.com>
2014-12-03 02:15:05 -0600

    going live with new index

--- application.py
+++ application.py
@@ -8,7 +8,7 @@ application.debug=True
  
 @application.route('/')
 def hello_world():
-    return "We stand on the shoulders of giants."
+    return flask.render_template('index.html')
  
 if __name__ == '__main__':
     application.run(host='0.0.0.0')
