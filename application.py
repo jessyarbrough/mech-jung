f73de257fa5b0a9b64f64176089741536baae6c6
Jess Yarbrough <jessyarbrough@hotmail.com>
2014-12-03 13:49:12 -0600

    making classifiers global

--- application.py
+++ application.py
@@ -1,11 +1,30 @@
-import flask, pickle
-# import numpy, scipy, sklearn, tweepy
+import flask
+# import pickle, numpy, scipy, sklearn, tweepy
+from pickle import loads as pkl_load
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.svm import SVC
 from sklearn.pipeline import Pipeline
 from sklearn.grid_search import GridSearchCV
  
 application = flask.Flask(__name__)
+
+prefs = ['ie', 'ns', 'ft', 'jp']
+clf_types = ['svc', 'nb', 'knn']
+doc_types = ['text', 'tweet']
+classifiers = {}
+
+for pref in prefs:
+	classifiers[pref] = {}
+	for clf_type in clf_types:
+		classifiers[pref][clf_type] = {}
+		for doc_type in doc_types:
+			classifiers[pref][clf_type][doc_type] = None
+			try:
+				fname = pref + '.' + clf_type + '.' + doc_type + '.pkl'
+				clf = pkl_load(open(fname).read())
+				classifiers[pref][clf_type][doc_type] = clf
+			except IOError:
+				pass
  
 @application.route('/')
 def hello_world():
@@ -14,13 +33,11 @@ def hello_world():
 @application.route('/results', methods = ['POST'])
 def classify():
 	doc = flask.request.form['text']
-	codes = ['ie', 'ns', 'ft', 'jp']
-	results = ''
-	for code in codes:
-		classifier = pickle.loads(open(code+'.svc.opt.pkl').read())
-		result = classifier.predict([doc])[0]
-		results = results + result
-	return results
+	result = ''
+	for pref in prefs:
+		clf = classifiers[pref]['svc']['text']
+		result += clf.predict([doc])[0]
+	return result
  
 if __name__ == '__main__':
 	application.debug = True
