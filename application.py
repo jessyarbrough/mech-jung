44bc36928f48a5e42b08be6b0dd3668b925d709b
Jess Yarbrough <jessyarbrough@hotmail.com>
2014-12-03 03:20:35 -0600

    full stack working

--- application.py
+++ application.py
@@ -1,14 +1,27 @@
-import flask, numpy, scipy, sklearn, tweepy
+import flask, pickle
+# import numpy, scipy, sklearn, tweepy
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.svm import SVC
+from sklearn.pipeline import Pipeline
+from sklearn.grid_search import GridSearchCV
  
 application = flask.Flask(__name__)
-
-#Set application.debug=true to enable tracebacks on Beanstalk log output. 
-#Make sure to remove this line before deploying to production.
-application.debug=True
  
 @application.route('/')
 def hello_world():
     return flask.render_template('index.html')
+
+@application.route('/results', methods = ['POST'])
+def classify():
+	doc = flask.request.form['text']
+	codes = ['ie', 'ns', 'ft', 'jp']
+	results = ''
+	for code in codes:
+		classifier = pickle.loads(open(code+'.svc.opt.pkl').read())
+		result = classifier.predict([doc])[0]
+		results = results + result
+	return results
  
 if __name__ == '__main__':
-    application.run(host='0.0.0.0')
+	application.debug = True
+	application.run(host='0.0.0.0')
