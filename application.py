bb2006cb9f8463e3580ef97d1944184628bb3a86
ulandgrant <ulandgrant@tamu.edu>
2014-12-03 20:57:55 -0600

    People Commit

--- application.py
+++ application.py
@@ -25,6 +25,19 @@ for pref in prefs:
 				classifiers[pref][clf_type][doc_type] = clf
 			except IOError:
 				pass
+
+personalities = {}
+
+with open("Personalities.tsv", "rb") as f:
+    for line in f:
+        line = line.split('\t')
+        key = line[1]
+        values = [line[0], line[2].strip()]
+        if key in personalities.keys():
+            personalities[key] += [[values]]
+        else:
+            personalities[key] = [[values]]
+
  
 @application.route('/')
 def hello_world():
@@ -37,7 +50,18 @@ def classify():
 	for pref in prefs:
 		clf = classifiers[pref]['svc']['text']
 		result += clf.predict([doc])[0]
-	return flask.render_template('results.html', result=result)
+    people = personalities[result]
+    length = len(people)
+    rand1 = random.randint(0, length/3)
+    rand2 = random.randint(length/3+1, 2*length/3)
+    rand3 = random.randint(2*length/3+1, length-1)
+    name1 = people[rand1][0]
+    pickurl1 = people[rand1][1]
+    name2 = people[rand2][0]
+    pickurl2 = people[rand2][1]
+    name3 = people[rand3][0]
+    pickurl3 = people[rand3][1]
+	return flask.render_template('results.html', result=result, name1=name1, name2=name2, name3=name3, pickurl1=pickurl1, pickurl2=pickurl2, pickurl3=pickurl3)
  
 if __name__ == '__main__':
 	application.debug = True
