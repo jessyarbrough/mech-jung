60252513bbb14e8efb34d2c18cbfc815cff206ea
edciri116 <edciri.116@gmail.com>
2014-12-03 19:41:52 -0600

    new things!

--- application.py
+++ application.py
@@ -37,7 +37,7 @@ def classify():
 	for pref in prefs:
 		clf = classifiers[pref]['svc']['text']
 		result += clf.predict([doc])[0]
-	return result
+	return flask.render_template('results.html', result=result)
  
 if __name__ == '__main__':
 	application.debug = True
