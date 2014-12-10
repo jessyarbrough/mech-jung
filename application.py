329992e6d9fa79f75410aa192dd69e84cf31d386
Jess Yarbrough <jessyarbrough@hotmail.com>
2014-12-09 21:20:34 -0600

    commit

--- application.py
+++ application.py
@@ -5,13 +5,13 @@ from sklearn.svm import SVC
 from sklearn.pipeline import Pipeline
 from sklearn.grid_search import GridSearchCV
 from preprocessor import transform, processes as lang_processes
-from nltk import Downloader as NltkDownloader
+from nltk.downloader import Downloader as NltkDownloader
 from build_committee import param_sets, prefs as bc_prefs
 
-nltk_dl = NltkDownloader()
 nltk_dir = '/usr/lib/nltk_data'
+nltk_dl = NltkDownloader(download_dir = nltk_dir)
 if __name__ == '__main__':
-	nltk_dir = nltk_dl.default_download_dir()
+	nltk_dl = NltkDownloader()
 nltk_packages = [
 	'punkt',
 	'maxent_treebank_pos_tagger',
